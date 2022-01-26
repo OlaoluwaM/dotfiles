bee001a1d248b8dd882a9c2965152bf53742bec9
OlaoluwaM <37044906+OlaoluwaM@users.noreply.github.com>
2022-01-26 12:46:38 +0100

    chore: Updated folder creation script

--- createDesinationsFile.sh
+++ createDesinationsFile.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 for d in */; do
-  ! [[ -f "${d}/destinations.json" ]] && touch "${d}/destinations.json"
+  ! [[ -f "${d}/destinations.json" ]] && echo "{}" > "${d}/destinations.json"
 done
 echo 'DONE!'
