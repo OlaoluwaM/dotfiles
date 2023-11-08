84e1319870bce4dbba78d76ee34a941a772837f8
Olaoluwa Mustapha <37044906+OlaoluwaM@users.noreply.github.com>
2023-11-08 04:56:14 -0600

    feat: Updated date widgets

--- .config/ags/js/bar/buttons/DateButton.js
+++ .config/ags/js/bar/buttons/DateButton.js
@@ -1,10 +1,11 @@
-import Clock from '../../misc/Clock.js';
-import PanelButton from '../PanelButton.js';
-import { App } from '../../imports.js';
+import Clock from "../../misc/Clock.js";
+import PanelButton from "../PanelButton.js";
+import { App } from "../../imports.js";
 
-export default ({ format = '%H:%M - %A %e.' } = {}) => PanelButton({
-    class_name: 'dashboard panel-button',
-    onClicked: () => App.toggleWindow('dashboard'),
-    window: 'dashboard',
+export default ({ format = "%A %B%e ~ %H:%M" } = {}) =>
+  PanelButton({
+    class_name: "dashboard panel-button",
+    onClicked: () => App.toggleWindow("dashboard"),
+    window: "dashboard",
     content: Clock({ format }),
-});
+  });
--- .config/ags/js/dashboard/DateColumn.js
+++ .config/ags/js/dashboard/DateColumn.js
@@ -1,50 +1,26 @@
-import icons from '../icons.js';
-import Clock from '../misc/Clock.js';
-import * as vars from '../variables.js';
-import { Widget } from '../imports.js';
-import Theme from '../services/theme/theme.js';
+import icons from "../icons.js";
+import Clock from "../misc/Clock.js";
+import * as vars from "../variables.js";
+import { Widget } from "../imports.js";
+import Theme from "../services/theme/theme.js";
 
-const SysProgress = (type, title, unit) => Widget.Box({
-    class_name: `circular-progress-box ${type}`,
-    hexpand: true,
-    binds: [['tooltipText', vars[type], 'value', v =>
-        `${title}: ${Math.floor(v * 100)}${unit}`]],
-    child: Widget.CircularProgress({
-        hexpand: true,
-        class_name: `circular-progress ${type}`,
-        child: Widget.Icon(icons.system[type]),
-        start_at: 0.75,
-        binds: [['value', vars[type]]],
-        connections: [[Theme, prog => {
-            prog.rounded = Theme.getSetting('radii') > 0;
-        }]],
-    }),
-});
-
-export default () => Widget.Box({
+export default () =>
+  Widget.Box({
     vertical: true,
-    class_name: 'datemenu',
+    class_name: "datemenu",
     children: [
-        Clock({ format: '%H:%M' }),
-        Widget.Label({
-            binds: [['label', vars.uptime, 'value', t => `uptime: ${t}`]],
-        }),
-        Widget.Box({
-            class_name: 'calendar',
-            children: [
-                Widget.Calendar({
-                    hexpand: true,
-                    hpack: 'center',
-                }),
-            ],
-        }),
-        Widget.Box({
-            class_name: 'system-info',
-            children: [
-                SysProgress('cpu', 'Cpu', '%'),
-                SysProgress('ram', 'Ram', '%'),
-                SysProgress('temp', 'Temperature', '°'),
-            ],
-        }),
+      Clock({ format: "%H:%M" }),
+      Widget.Label({
+        binds: [["label", vars.uptime, "value", (t) => `Uptime: ${t}`]],
+      }),
+      Widget.Box({
+        class_name: "calendar",
+        children: [
+          Widget.Calendar({
+            hexpand: true,
+            hpack: "center",
+          }),
+        ],
+      }),
     ],
-});
+  });
