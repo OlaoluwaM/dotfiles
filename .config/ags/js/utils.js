f15cd7b3467c8991037d81fa15fb8b720ee3df02
Olaoluwa Mustapha <37044906+OlaoluwaM@users.noreply.github.com>
2023-11-08 18:38:07 -0600

    chore: update

--- .config/ags/js/utils.js
+++ .config/ags/js/utils.js
@@ -121,7 +121,7 @@ export function scssWatcher() {
       "--event",
       "create,modify",
       "-m",
-      `/home/${Utils.USER}/Desktop/olaolu_dev/dotfiles/.config/ags/scss`,
+      `/home/${Utils.USER}/Desktop/dotfiles/.config/ags/scss`,
     ],
     () => Theme.setup(),
     () => print("missing dependancy for css hotreload: inotify-tools")
