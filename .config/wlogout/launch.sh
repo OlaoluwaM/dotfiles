c6e665933b1cba1a50e4fa9ab45d3d60136a85a6
Olaoluwa Mustapha <37044906+OlaoluwaM@users.noreply.github.com>
2023-11-23 10:49:56 -0600

    chore: dotfiles update!

--- .config/wlogout/launch.sh
+++ .config/wlogout/launch.sh
@@ -3,15 +3,8 @@
 LAYOUT="$HOME/.config/wlogout/layout"
 STYLE="$HOME/.config/wlogout/style.css"
 
-if [[ ! `pidof wlogout` ]]; then
-    wlogout --layout ${LAYOUT} --css ${STYLE} \
-        --buttons-per-row 3 \
-        --column-spacing 50 \
-        --row-spacing 50 \
-        --margin-top 290 \
-        --margin-bottom 290 \
-        --margin-left 300 \
-        --margin-right 300
+if [[ ! $(pidof wlogout) ]]; then
+    wlogout
 else
     pkill wlogout
 fi
