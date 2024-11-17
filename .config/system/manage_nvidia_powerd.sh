fd8554aa89bdcfa8aba71c14efaaf93cc46d5413
Olaoluwa Mustapha <37044906+OlaoluwaM@users.noreply.github.com>
2024-11-17 08:39:02 -0600

    feat: Purge and cleanup dots

--- .config/system/manage_nvidia_powerd.sh
+++ .config/system/manage_nvidia_powerd.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 # This should be placed in /usr/local/bin
+# This is being used by the corresponding udev rule located at '/etc/udev/rules.d/99-manage-nvidia-powerd.rules'
+# That rule, '99-manage-nvidia-powerd.rules', references this script when it is located at '/usr/local/bin/'
+# These two, this script and the udev rule, exist to turn off the 'nvidia_powerd' service while the system runs on battery power and turn it back on while the system is on AC
+# This is important because the 'nvidia_powerd' service works to improve the performance (and I think efficiency) of our nvidia gpu and thus siphons away more power for this purpose.
+# While the system is on AC, this is not a problem (hence why it can be on), but when using battery power, this somewhat greatly reduces battery life, which is not ideal
+
+# IMPORTANT: This script and its corresponding udev rule cannot be symlinked and must instead be copied to their required destinations.
 
 SERVICE_NAME="nvidia-powerd"
 LOG_TAG="manage_nvidia_powerd"
