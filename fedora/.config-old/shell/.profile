fe2431c3eab8f55e06509478edd1c176b4fe1d1c
Olaoluwa Mustapha <37044906+OlaoluwaM@users.noreply.github.com>
2026-05-03 16:57:57 -0500

    refactor: Delete ./fedora/.config-old
    Removed since it will continuously drift from ./fedora/.config. It's
    value was ensuring a previous operation was done correctly, but that
    diminishes over time so let's remove it outright

--- fedora/.config-old/shell/.profile
+++
@@ -1,3 +0,0 @@
-. "$HOME/.cargo/env"
-
-[ -f "/home/olaolu/.ghcup/env" ] && . "/home/olaolu/.ghcup/env" # ghcup-env
--- fedora/.config-old/shell/scripts/augment-path-var.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-pathsToAdd=(".local/bin" "go/bin")
-
-for pathToDir in "${pathsToAdd[@]}"; do
-   if ! echo $PATH | grep -q "$HOME/$pathToDir"; then
-      [ -d "$HOME/$pathToDir" ] && PATH="$PATH:$HOME/$pathToDir"
-   fi
-done
